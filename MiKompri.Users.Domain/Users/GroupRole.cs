4213fe7333bd5e960f17b9c347c8bb59285a9d9a
frank cv <frankinginfo2013@gmail.com>
2025-12-04 16:12:23 +0100

    Closed 22

---
+++ MiKompri.Users.Domain/Users/GroupRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiKompri.Users.Domain.Users
+{
+    public enum GroupRole
+    {
+        Owner = 1,
+        Member = 2
+    }
+}
---
+++ MiKompri.Users.Domain/Users/IGroupMembershipRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiKompri.Users.Domain.Users
+{
+    public interface IGroupMembershipRepository
+    {
+        Task<IReadOnlyCollection<GroupMembership>> GetByGroupIdAsync(
+            Guid groupId,
+            CancellationToken cancellationToken = default);
+
+        Task<IReadOnlyCollection<GroupMembership>> GetByUserIdAsync(
+            Guid userId,
+            CancellationToken cancellationToken = default);
+    }
+}
