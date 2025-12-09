7ce92804dcb559ddd06b2e380d91d8e269d36754
frank cv <frankinginfo2013@gmail.com>
2025-12-09 11:06:51 +0100

    closed 29

---
+++ MiKompri.Users.Application/Commands/AddMemberToGroup/AddMemberToGroupCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using MiKompri.Users.Domain.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiKompri.Users.Application.Commands.AddMemberToGroup
+{
+    public sealed record AddMemberToGroupCommand(
+    Guid GroupId,
+    Guid MemberUserId,
+    GroupRole Role
+) : IRequest;
+}
